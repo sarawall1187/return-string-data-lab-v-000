79af664664e3dee4b98ab3a1a7a3de68845167da
Sara Walker <sarawall1187@gmail.com>
2019-06-24 17:40:58 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190624165416) do
+ActiveRecord::Schema.define(version: 20190624174026) do
 
   create_table "customers", force: :cascade do |t|
     t.string   "name"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20190624165416) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description"
-    t.string   "inventory"
+    t.integer  "inventory"
   end
 
 end
