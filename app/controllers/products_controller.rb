c82d2e87e03e89fc5e6c96c46157a3b80f8403d3
Sara Walker <sarawall1187@gmail.com>
2019-06-24 17:51:20 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,7 @@ class ProductsController < ApplicationController
   end
 
   def create
-    Product.create(product_params)
+    @product = Product.create(product_params)
     redirect_to products_path
   end
 
